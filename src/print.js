8fbe1416b9e5ba9afb115a29b85127eb6e1269cc
pakizheng <pakizheng@tencent.com>
2020-10-18 19:48:36 +0800

    增加文件

---
+++ src/print.js
@@ -0,0 +1,5 @@
+export default function printMe() {
+  const a = 'hhh'
+  const b = 'jjjj'
+  console.log(a, b, 'I get called from print.js!');
+}
---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const ManifestPlugin = require('webpack-manifest-plugin')
+
+module.exports = {
+  entry: {
+    app: './src/index.js',
+    print: './src/print.js',
+  },
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: '/'
+  },
+  devtool: 'inline-source-map',
+  devServer: {
+    contentBase: './dist',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [{ loader: 'style-loader' }, { loader: 'css-loader' }],
+      },
+    ],
+  },
+  plugins: [
+    new CleanWebpackPlugin({ cleanStaleWebpackAssets: false }),
+    new HtmlWebpackPlugin({
+      title: 'Output Management',
+    }),
+    new ManifestPlugin(),
+  ],
+}
